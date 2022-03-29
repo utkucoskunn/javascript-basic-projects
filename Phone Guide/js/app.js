b728a75ce96918db4bad6c9ae8d1b43e9a417fe4
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-03-30 00:02:42 +0300

    UI finished!

--- Phone Guide/js/app.js
+++ Phone Guide/js/app.js
@@ -1,37 +1,66 @@
-class person{
-    constructor(name,surname,mail) {
-        this.name=name;
-        this.surname=surname;
-        this.mail=mail;
+class Kisi {
+    constructor(ad, soyad, mail) {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.mail = mail;
     }
 }
-
-class UI{
+class Util{
+    static bosAlanKontrolEt(...alanlar){
+    let sonuc=true;
+    alanlar.forEach(alan=>{
+        if(alan.length===0){
+            sonuc=false;
+            return false;
+        }
+    });
+    return sonuc;
+    }
+}
+class Ekran {
     constructor() {
-        this.name=document.getElementById('name');
-        this.surname=document.getElementById('surname');
-        this.mail=document.getElementById('mail');
-        this.ekleGuncelleeButton=document.querySelector('.btn')
-        this.depo=new Depo();
+        this.ad = document.getElementById('.ad')
+        this.soyad = document.getElementById('soyad');
+        this.mail = document.getElementById('mail');
+        this.ekleGuncelleButton = document.querySelector('.btn')
+        this.form = document.getElementById("form-rehber").addEventListener('submit', this.kaydetGuncelle);
+        this.depo = new Depo();
+    }
+
+    kaydetGuncelle(e) {
+        e.preventDefault();
+        const kisi = new Kisi(this.ad.value, this.soyad.value, this.mail.value);
+        const sonuc=Util.bosAlanKontrolEt(kisi.ad, kisi.soyad, kisi.mail)
+        console.log(sonuc);
+
+
     }
 }
 
 class Depo {
     constructor() {
-        this.tumKisiler=[];
+        this.tumKisiler = [];
 
     }
-    verileriGetir(){
+
+    kisileriGetir() {
         let tumKisilerLocal;
-        if (localStorage.getItem('tumKisiler')===null){
-       tumKisilerLocal=[];
-        }else {
-            tumKisilerLocal=JSON.parse(localStorage.getItem('tumKisiler'));
+        if (localStorage.getItem('tumKisiler') === null) {
+            tumKisilerLocal = [];
+        } else {
+            tumKisilerLocal = JSON.parse(localStorage.getItem('tumKisiler'));
         }
-        this.tumKisiler=tumKisilerLocal;
+        this.tumKisiler = tumKisilerLocal;
+        return tumKisilerLocal;
     }
 
-    kisiEkle(kisi){
-        const tumKisilerLocal=JSON
+    kisiEkle(kisi) {
+        const tumKisilerLocal = this.kisileriGetir();
+        tumKisilerLocal.push(kisi);
+        localStorage.setItem('tumKisiler', JSON.stringify(tumKisilerLocal))
     }
 }
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    const ekran = new Ekran();
+});
