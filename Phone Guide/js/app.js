754d9ddef439e2db885cbf10f96dcecb395424f4
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-03-29 19:30:09 +0300

    Part! finished!

--- Phone Guide/js/app.js
+++ Phone Guide/js/app.js
@@ -1,74 +1,37 @@
-const name = document.getElementById('name');
-const surname = document.getElementById('surname');
-const mail = document.getElementById('mail');
-const info = document.querySelector(".info");
-const form = document.querySelector('.btn');
-const personList = document.querySelector('.person-list');
+class person{
+    constructor(name,surname,mail) {
+        this.name=name;
+        this.surname=surname;
+        this.mail=mail;
+    }
+}
 
-form.addEventListener("click", save);
+class UI{
+    constructor() {
+        this.name=document.getElementById('name');
+        this.surname=document.getElementById('surname');
+        this.mail=document.getElementById('mail');
+        this.ekleGuncelleeButton=document.querySelector('.btn')
+        this.depo=new Depo();
+    }
+}
 
-const tumKisilerDizi=[];
-//*****************************************************************************************
+class Depo {
+    constructor() {
+        this.tumKisiler=[];
 
-function save(e) {
-    e.preventDefault();
-
-    const person = {
-        name: name.value,
-        surname: surname.value,
-        mail: mail.value
+    }
+    verileriGetir(){
+        let tumKisilerLocal;
+        if (localStorage.getItem('tumKisiler')===null){
+       tumKisilerLocal=[];
+        }else {
+            tumKisilerLocal=JSON.parse(localStorage.getItem('tumKisiler'));
+        }
+        this.tumKisiler=tumKisilerLocal;
     }
 
-    const durum = control(person);
-    bilgiolustur(durum);
-   addPerson(person);
-
-}
-
-const addPerson = (person) => {
-    const trElement = document.createElement('tr');
-    trElement.innerHTML =
-        `<td>${person.name}</td>
-        <td>${person.surname}</td>
-        <td>${person.mail}</td>
-        <td>  
-        <button class="btn-edit"> <i class="bi bi-pencil-square"></i></button>
-        <button class="btn-delete"><i class="bi bi-trash"></i></button> 
-        </td>`;
-
-    document.querySelector(".table").appendChild(trElement);
-    tumKisilerDizi.push(person);
-}
-
-function control(person) {
-    let result;
-    for (let info in person) {
-        result = !!person[info];
+    kisiEkle(kisi){
+        const tumKisilerLocal=JSON
     }
-    return result;
-}
-
-const bilgiolustur = (durum) => {
-
-    const olusturulanBilgi = document.createElement('div')
-    olusturulanBilgi.className = 'alert';
-
-    if (durum) {
-        olusturulanBilgi.classList.add('col-sm-6');
-        olusturulanBilgi.textContent = "Kayıt Başarılı";
-        olusturulanBilgi.classList.add('alert-success');
-    } else {
-        olusturulanBilgi.classList.add('col-sm-6');
-        olusturulanBilgi.textContent = "Lütfen bütün alanları doldurunuz!";
-        olusturulanBilgi.classList.add('alert-danger');
-    }
-
-    document.querySelector(".main-container").appendChild(olusturulanBilgi);
-    setTimeout(function () {
-        document.querySelector('.alert').remove();
-    }, 2000)
-    labelCleaner();
-}
-const labelCleaner = () => {
-
 }
